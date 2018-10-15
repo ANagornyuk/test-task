48acc45d626481b57bedb55b154799685a418c59
A N <qqq.example.com>
2018-10-15 16:12:52 +0300

    output table done

--- files_table.php
+++ files_table.php
@@ -8,7 +8,14 @@
 	    echo "<table><tr><th>Name</th><th>Type</th><th>Size</th><th>Upload time</th><th>Download</th><th>Delete</th></tr>";
 	    // output data of each row
 	    while($row = $result->fetch_assoc()) {
-	        echo "<tr><td>".$row["name"]."</td><td>".$row["type"]." ".strval(intval($row["size"])/1024)."Kb"."</td><td>".date("Y-m-d H:i:s", $row["upl_time"])."</td><td><a href=".$row["path"]." download>Скачать файл</a></td><td></td></tr>";
+	        echo "<tr>
+	        <td>".$row["name"]."</td>
+	        <td>".$row["type"]."</td>
+	        <td>".strval(intval($row["size"])/1024)."Kb"."</td>
+	        <td>".date("Y-m-d H:i:s", $row["upl_time"])."</td>
+	        <td><a href=".$row["path"]." download>Скачать файл</a></td>
+	        <td></td>
+	        </tr>";
 	    }
 	    echo "</table>";
 	} else {
