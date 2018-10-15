47c5967dc71d92d310592ce85196d01dff9c2195
A N <qqq.example.com>
2018-10-15 15:26:42 +0300

    trying to output uploaded files table

---
+++ files_table.php
@@ -0,0 +1,18 @@
+<?php
+	include 'mysql.php';
+
+	$sql = "SELECT name, path, type, size, upl_time FROM file";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+	    echo "<table><tr><th>Name</th><th>Type</th><th>Size</th><th>Upload time</th><th>Download</th><th>Delete</th></tr>";
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+	        echo "<tr><td>".$row["name"]."</td><td>".$row["type"]." ".strval(intval($row["size"])/1024)."Kb"."</td><td>".date("Y-m-d H:i:s", $row["upl_time"])."</td><td><a href=".$row["path"]." download>Скачать файл</a></td><td></td></tr>";
+	    }
+	    echo "</table>";
+	} else {
+	    echo "0 results";
+	}
+	//$conn->close();
+?>
