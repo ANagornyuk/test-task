34a3e1d914dce083750c68917d702068e1de4e6a
A N <qqq.example.com>
2018-10-16 14:16:55 +0300

    deleting done

--- delete_file.php
+++ delete_file.php
@@ -4,14 +4,14 @@ require 'mysql.php';
 
 
 
-if(!empty($row = $_POST["row"])){
-	$sql = "DELETE FROM adyax WHERE  id=".$row["id"]."";
+if(!empty($id = $_POST["id"])){
+	$sql = 'DELETE FROM file WHERE  id='.$id.'';
 	if ($conn->query($sql) === TRUE) {
     	echo "Record deleted successfully";
 	} else {
     	echo "Error deleting record: " . $conn->error;
 	}
-	unlink($row["path"]);
+	unlink($_POST["file_path"]);
 }
 
 
--- files_table.php
+++ files_table.php
@@ -7,6 +7,10 @@
 	if ($result->num_rows > 0) {
 	    echo "<table><tr><th>Name</th><th>Type</th><th>Size</th><th>Upload time</th><th>Download</th><th>Delete</th></tr>";
 	    // output data of each row
+	    $form = '<form method="POST" action="delete_file.php">
+		    	<input type="hidden" name="row" value="$row" />
+		    	<input type="submit" value="Удалить" />
+			</form>';
 	    while($row = $result->fetch_assoc()) {
 	        echo "<tr>
 	        <td>".$row["name"]."</td>
@@ -14,10 +18,11 @@
 	        <td>".strval(intval($row["size"])/1024)."Kb"."</td>
 	        <td>".date("Y-m-d H:i:s", $row["upl_time"])."</td>
 	        <td><a href=".$row["path"]." download>Скачать файл</a></td>
-	        <td><form method="POST" action="delete_file.php">
-		    	<input type="hidden" name="row" value="$row" />
+	        <td>".'<form method="POST" action="delete_file.php">
+		    	<input type="hidden" name="id" value="'.$row["id"].'" />
+		    	<input type="hidden" name="file_path" value="'.$row["path"].'" />
 		    	<input type="submit" value="Удалить" />
-			</form></td>
+			</form>'."</td>
 	        
 	        </tr>";
 	    }
